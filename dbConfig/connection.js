07dff837164a51909803d35010f62de45587ef0e
Pranjal Maurya <140903582+PranjalMaurya07@users.noreply.github.com>
2024-11-23 01:40:47 +0530

    Update connection.js

--- dbConfig/connection.js
+++ dbConfig/connection.js
@@ -4,7 +4,8 @@ const mysqlPool = mysql.createPool({
     host : 'localhost',
     user : 'root',
     password : process.env.DB_PASSWORD,
-    database : 'school_db'
+    database : 'school_db',
+    port : 3306
 })
 
 module.exports = {
